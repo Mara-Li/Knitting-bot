ec9b2b567c6cefd3e708954ea98351ad71f322ac
Mara <mara-li@outlook.fr>
2023-04-29 19:25:27 +0200

    refactor: improve message logging

--- src/listeners/onChannelUpdate.ts
+++ src/listeners/onChannelUpdate.ts
@@ -1,4 +1,4 @@
-import { ChannelType, Client } from "discord.js";
+import { Channel, ChannelType, Client, Snowflake, TextChannel } from 'discord.js';
 
 /**
  * @param {Client} client - Discord.js Client
@@ -7,17 +7,19 @@ import { ChannelType, Client } from "discord.js";
  * When the permission of a channel is updated, check if the channel have thread and update them.
  */
 
+
+
 export default (client: Client): void => {
-	client.on("channelUpdate", async (oldChannel, newChannel) => {
-		console.log(`Channel ${oldChannel.id} updated!`);
-		if (oldChannel.type !== ChannelType.GuildText || newChannel.type !== ChannelType.GuildText)
+	client.on("channelUpdate", async (
+		oldChannel,
+		newChannel) => {
+		console.log(`Channel #${getChannelName(oldChannel.id, client)} updated`);
+		if (oldChannel.type !== ChannelType.GuildText
+			|| newChannel.type !== ChannelType.GuildText
+			|| oldChannel.permissionOverwrites.cache === newChannel.permissionOverwrites.cache) {
+			console.log("Channel is not a text channel or permission are not changed");
 			return;
-		//check if the permission of the channel is updated
-		if (
-			oldChannel.permissionOverwrites.cache.size ===
-            newChannel.permissionOverwrites.cache.size
-		)
-			return;
+		}
 		//get all threads of this channel
 		console.log(`Updating threads of ${newChannel.name}`);
 		const threads = await newChannel.threads.cache;
@@ -52,3 +54,16 @@ export default (client: Client): void => {
 		});
 	});
 };
+
+
+/**
+ * @description Get the name of a channel
+ * @param channelID {Snowflake} - The ID of the channel
+ * @param Client {Client} - The Discord.js Client
+ */
+function getChannelName(channelID: Snowflake, Client: Client) {
+	const channel = Client.channels.cache.get(channelID);
+	//check if the channel is a text channel
+	if (!channel || channel.type !== ChannelType.GuildText) return channelID;
+	return (channel as TextChannel).name;
+}
