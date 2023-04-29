622cc061bd2a5a289b17fc9899da7cd801f49b10
Mara <mara-li@outlook.fr>
2023-04-29 17:59:28 +0200

    feat: onMemberUpdate, trigger only when role change

--- src/listeners/memberUpdate.ts
+++ src/listeners/memberUpdate.ts
@@ -1,11 +1,11 @@
-import { Client, ThreadMember } from 'discord.js';
-import { TextChannel, ThreadChannel } from 'discord.js';
+import { Client, ThreadMember, ThreadChannel } from 'discord.js';
 
 export default (client: Client): void => {
     client.on('guildMemberUpdate', async (oldMember, newMember) => {
+        //trigger only on role change
+        if (oldMember.roles.cache.size === newMember.roles.cache.size) return;
         console.log(`${oldMember.user.username} has been updated!`);
         const guild = newMember.guild;
-        const memberTotal = guild.memberCount;
         const channels = guild.channels.cache.filter(channel => channel.isThread());
         for (const channel of channels.values()) {
             const ThreadChannel = channel as ThreadChannel;
@@ -14,6 +14,15 @@ export default (client: Client): void => {
             threadMembers.forEach(member => {
                 threadMemberArray.push(member);
             });
+            //check if user not in thread
+            if (!threadMemberArray.some(member => member.id === newMember.id)) {
+                //check thread permission with user role
+                if (ThreadChannel.permissionsFor(newMember).has('ViewChannel')) {
+                    //add user to thread
+                    ThreadChannel.members.add(newMember);
+                    console.log(`Added ${newMember.user.username} to ${ThreadChannel.name}`);
+                }
+            }
         }
     });
 };
