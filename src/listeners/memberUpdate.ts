2c84dcda0154d6c898e3e7ea7813926ec2c593f9
Mara <mara-li@outlook.fr>
2023-04-29 18:51:23 +0200

    fix: remove user when role change

--- src/listeners/memberUpdate.ts
+++ src/listeners/memberUpdate.ts
@@ -8,8 +8,8 @@ export default (client: Client): void => {
 		const guild = newMember.guild;
 		const channels = guild.channels.cache.filter(channel => channel.isThread());
 		for (const channel of channels.values()) {
-			const ThreadChannel = channel as ThreadChannel;
-			const threadMembers = await ThreadChannel.members.fetch();
+			const threadChannel = channel as ThreadChannel;
+			const threadMembers = await threadChannel.members.fetch();
 			const threadMemberArray: ThreadMember<boolean>[] = [];
 			threadMembers.forEach(member => {
 				threadMemberArray.push(member);
@@ -17,10 +17,16 @@ export default (client: Client): void => {
 			//check if user not in thread
 			if (!threadMemberArray.some(member => member.id === newMember.id)) {
 				//check thread permission with user role
-				if (ThreadChannel.permissionsFor(newMember).has("ViewChannel")) {
+				if (threadChannel.permissionsFor(newMember).has("ViewChannel")) {
 					//add user to thread
-					ThreadChannel.members.add(newMember);
-					console.log(`Added ${newMember.user.username} to ${ThreadChannel.name}`);
+					await threadChannel.members.add(newMember);
+					console.log(`Added ${newMember.user.username} to ${threadChannel.name}`);
+				}
+			} //remove user from thread if not have permission
+			else {
+				if (!threadChannel.permissionsFor(newMember).has("ViewChannel")) {
+					await threadChannel.members.remove(newMember.id);
+					console.log(`Removed ${newMember.user.username} from ${threadChannel.name}`);
 				}
 			}
 		}
