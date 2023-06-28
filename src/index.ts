face349f2b7215f98355d24050e1ecff14e4aafe
Mara <mara-li@outlook.fr>
2023-06-28 17:17:20 +0200

    refactor: remove emoji/message

--- src/index.ts
+++ src/index.ts
@@ -23,9 +23,6 @@ const client = new Client({
 	partials: [Partials.Channel],
 });
 
-export const emoji = process.env.MESSAGE && process.env.MESSAGE.trim().length > 0 ? process.env.MESSAGE : "🔄";
-
-
 try {
 	ready(client);
 	memberUpdate(client);
