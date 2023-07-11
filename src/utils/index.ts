2ac745e5a7fa2619a3915e4543eee9572ca30d43
Mara <mara-li@outlook.fr>
2023-07-11 19:42:15 +0200

    refactor: add stack for logIndev

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -4,11 +4,15 @@ import process from "process";
 export function logInDev(...text: unknown[]) {
 	const time= new Date();
 	const timeString = `${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}`;
+	/** get the called function name */
+	const stack = new Error().stack;
+	const caller = stack?.split("\n")[2].trim().split(" ")[1];
+	
 	if (process.env.NODE_ENV === "development") {
 		if (text.length === 1 && typeof text[0] === "string") {
-			console.log(`${timeString} - ${text}`);
+			console.log(`${timeString} (${caller}) - ${text}`);
 		} else {
-			console.log(timeString, text);
+			console.log(`${timeString} (${caller}`, text);
 		}
 	}
 }
