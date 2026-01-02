2a75c3449725444037ca616a03295dc9f605e033
Mara-Li <lili.simonetti@outlook.fr>
2026-01-02 23:05:43 +0100

    fix: the channel was never found as they was a typo error

--- src/events/delete/onChannelDelete.ts
+++ src/events/delete/onChannelDelete.ts
@@ -1,15 +1,14 @@
 import {
-	CategoryChannel,
+	type CategoryChannel,
 	ChannelType,
-	Client,
+	type Client,
 	DMChannel,
-	ForumChannel,
-	NonThreadGuildBasedChannel,
-	TextChannel,
+	type ForumChannel,
+	type NonThreadGuildBasedChannel,
+	type TextChannel,
 } from "discord.js";
 import { TypeName } from "../../interface";
 import { getMaps, getRoleIn, setIgnore, setRoleIn } from "../../maps";
-import { logInDev } from "../../utils";
 
 /**
  * @param {Client} client - Discord.js Client
@@ -37,9 +36,7 @@ export default (client: Client): void => {
 			const ignored = getRoleIn("ignore", guildID);
 			ignored.forEach((ignored) => {
 				const channels = ignored.channels;
-				const index = channels.findIndex(
-					(channel) => channel.id === channel.id,
-				);
+				const index = channels.findIndex((chan) => chan.id === channel.id);
 				channels.splice(index, 1);
 				ignored.channels = channels;
 			});
@@ -50,9 +47,7 @@ export default (client: Client): void => {
 			const followed = getRoleIn("follow", guildID);
 			followed.forEach((followed) => {
 				const channels = followed.channels;
-				const index = channels.findIndex(
-					(channel) => channel.id === channel.id,
-				);
+				const index = channels.findIndex((chan) => chan.id === channel.id);
 				channels.splice(index, 1);
 				followed.channels = channels;
 			});
@@ -62,31 +57,17 @@ export default (client: Client): void => {
 			/**
 			 * Remove the channel from the database "follow" and "ignore" maps
 			 */
-			const allIgnore = getMaps(
-				"ignore",
-				TypeName.channel,
-				guildID,
-			) as TextChannel[];
-			const allFollow = getMaps(
-				"follow",
-				TypeName.channel,
-				guildID,
-			) as TextChannel[];
+			const allIgnore = getMaps("ignore", TypeName.channel, guildID) as TextChannel[];
+			const allFollow = getMaps("follow", TypeName.channel, guildID) as TextChannel[];
 			const isIgnored = allIgnore.some((ignored) => ignored.id === channel.id);
-			const isFollowed = allFollow.some(
-				(followed) => followed.id === channel.id,
-			);
+			const isFollowed = allFollow.some((followed) => followed.id === channel.id);
 			if (isIgnored) {
-				const index = allIgnore.findIndex(
-					(ignored) => ignored.id === channel.id,
-				);
+				const index = allIgnore.findIndex((ignored) => ignored.id === channel.id);
 				allIgnore.splice(index, 1);
 				setIgnore(TypeName.channel, guildID, allIgnore as TextChannel[]);
 			}
 			if (isFollowed) {
-				const index = allFollow.findIndex(
-					(followed) => followed.id === channel.id,
-				);
+				const index = allFollow.findIndex((followed) => followed.id === channel.id);
 				allFollow.splice(index, 1);
 				setIgnore(TypeName.channel, guildID, allFollow);
 			}
@@ -97,29 +78,27 @@ export default (client: Client): void => {
 			const allCategoryIgnore = getMaps(
 				"ignore",
 				TypeName.category,
-				guildID,
+				guildID
 			) as CategoryChannel[];
 			const allCategoryFollow = getMaps(
 				"follow",
 				TypeName.category,
-				guildID,
+				guildID
 			) as CategoryChannel[];
 			const isCategoryIgnored = allCategoryIgnore.some(
-				(ignored) => ignored.id === channel.id,
+				(ignored) => ignored.id === channel.id
 			);
 			const isCategoryFollowed = allCategoryFollow.some(
-				(followed) => followed.id === channel.id,
+				(followed) => followed.id === channel.id
 			);
 			if (isCategoryIgnored) {
-				const index = allCategoryIgnore.findIndex(
-					(ignored) => ignored.id === channel.id,
-				);
+				const index = allCategoryIgnore.findIndex((ignored) => ignored.id === channel.id);
 				allCategoryIgnore.splice(index, 1);
 				setIgnore(TypeName.category, guildID, allCategoryIgnore);
 			}
 			if (isCategoryFollowed) {
 				const index = allCategoryFollow.findIndex(
-					(followed) => followed.id === channel.id,
+					(followed) => followed.id === channel.id
 				);
 				allCategoryFollow.splice(index, 1);
 				setIgnore(TypeName.category, guildID, allCategoryFollow);
@@ -131,30 +110,24 @@ export default (client: Client): void => {
 			const allThreadIgnore = getMaps(
 				"ignore",
 				TypeName.forum,
-				guildID,
+				guildID
 			) as ForumChannel[];
 			const allThreadFollow = getMaps(
 				"follow",
 				TypeName.forum,
-				guildID,
+				guildID
 			) as ForumChannel[];
-			const isForumIgnored = allThreadIgnore.some(
-				(ignored) => ignored.id === channel.id,
-			);
+			const isForumIgnored = allThreadIgnore.some((ignored) => ignored.id === channel.id);
 			const isForumFollowed = allThreadFollow.some(
-				(followed) => followed.id === channel.id,
+				(followed) => followed.id === channel.id
 			);
 			if (isForumIgnored) {
-				const index = allThreadIgnore.findIndex(
-					(ignored) => ignored.id === channel.id,
-				);
+				const index = allThreadIgnore.findIndex((ignored) => ignored.id === channel.id);
 				allThreadIgnore.splice(index, 1);
 				setIgnore(TypeName.forum, guildID, allThreadIgnore);
 			}
 			if (isForumFollowed) {
-				const index = allThreadFollow.findIndex(
-					(followed) => followed.id === channel.id,
-				);
+				const index = allThreadFollow.findIndex((followed) => followed.id === channel.id);
 				allThreadFollow.splice(index, 1);
 				setIgnore(TypeName.forum, guildID, allThreadFollow);
 			}
