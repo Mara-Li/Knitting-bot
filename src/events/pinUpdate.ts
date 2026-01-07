37e22f9a80a958123463e9438469e9054efba539
Mara-Li <lili.simonetti@outlook.fr>
2026-01-07 23:22:13 +0100

    fix: handle channelPinsUpdate event with improved time handling

--- src/events/pinUpdate.ts
+++ src/events/pinUpdate.ts
@@ -1,13 +1,16 @@
 import type { Client, TextBasedChannel } from "discord.js";
 
 export default (client: Client): void => {
-	client.on("channelPinsUpdate", async (channel: TextBasedChannel, time: Date) => {
-		//fetch the system message that triggered the pin update
-		const timeNb = time as unknown as number;
-		const systemMessage = (await channel.messages.fetch()).find((msg) => {
-			return msg.system && msg.createdTimestamp >= timeNb - 5000;
-		});
-		if (systemMessage && systemMessage.author === client.user)
-			await systemMessage.delete();
-	});
+	client.on(
+		"channelPinsUpdate",
+		async (channel: TextBasedChannel, time: Date | number) => {
+			//fetch the system message that triggered the pin update
+			const timeNb = typeof time === "number" ? time : time.getTime();
+			const systemMessage = (await channel.messages.fetch()).find((msg) => {
+				return msg.system && msg.createdTimestamp >= timeNb - 5000;
+			});
+			if (systemMessage && systemMessage.author === client.user)
+				await systemMessage.delete();
+		}
+	);
 };
