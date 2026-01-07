6c52c1fbe96388673610ef1149150ba6e3102c28
Mara-Li <lili.simonetti@outlook.fr>
2026-01-07 23:39:42 +0100

    fix(logs): notify admins when the bot's cached message is deleted

--- src/events/delete/onMessageDelete.ts
+++ src/events/delete/onMessageDelete.ts
@@ -1,12 +1,15 @@
 import { type Client, MessageFlags, type ThreadChannel } from "discord.js";
+import { getTranslation, ln } from "../../i18n";
 import { EMOJI } from "../../index";
 import {
 	deleteCachedMessage,
 	getCachedMessage,
+	getConfig,
 	getMessageToSend,
 	getPinSetting,
 	setCachedMessage,
 } from "../../maps";
+import { discordLogs } from "../../utils";
 
 /**
  * @param {Client} client - Discord.js Client
@@ -22,29 +25,15 @@ export default (client: Client): void => {
 		const guildId = message.guild.id;
 		const threadId = message.channel.id;
 		const cachedMessageId = getCachedMessage(guildId, threadId);
-
-		// If this deleted message was our cached message, recreate it
+		// If this deleted message was our cached message, warn the admins
 		if (cachedMessageId === message.id) {
 			deleteCachedMessage(guildId, threadId);
-
-			try {
-				const thread = message.channel as ThreadChannel;
-				const messageToSend = getMessageToSend(guildId) || EMOJI;
-				const shouldPin = getPinSetting(guildId);
-
-				// Recreate the bot message
-				const newMessage = await thread.send({
-					content: messageToSend,
-					flags: MessageFlags.SuppressNotifications,
-				});
-
-				if (shouldPin) await newMessage.pin();
-
-				// Update cache with new message
-				setCachedMessage(guildId, threadId, newMessage.id);
-			} catch (error) {
-				console.warn(`Failed to recreate bot message in thread ${threadId}:`, error);
-			}
+			const ul = getTranslation(guildId, { locale: message.guild.preferredLocale });
+			await discordLogs(
+				guildId,
+				client,
+				ul("error.deletedBotMessage", { bot: client.user.id, thread: threadId })
+			);
 		}
 	});
 };
