49de1abfd5bf8adbd2156c26c38667a10f305948
Mara-Li <lili.simonetti@outlook.fr>
2026-01-07 23:40:04 +0100

    fix(guild): remove clearing of message cache on guild deletion

--- src/events/onGuildQuit.ts
+++ src/events/onGuildQuit.ts
@@ -1,9 +1,8 @@
 import type { Client } from "discord.js";
-import { clearGuildMessageCache, deleteGuild } from "../maps";
+import { deleteGuild } from "../maps";
 
 export default (client: Client): void => {
 	client.on("guildDelete", async (guild) => {
 		deleteGuild(guild.id);
-		clearGuildMessageCache(guild.id);
 	});
 };
