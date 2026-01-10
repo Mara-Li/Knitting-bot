27f70c50d19aa1e6950eac4fa988a93a6e3bcdd7
Mara-Li <lili.simonetti@outlook.fr>
2026-01-10 23:44:37 +0100

    fix: emoji

--- src/interfaces/constant.ts
+++ src/interfaces/constant.ts
@@ -1,6 +1,8 @@
 import process from "node:process";
 import pkg from "../../package.json" with { type: "json" };
+import dotenv from "dotenv";
 
+dotenv.config({quiet: true});
 export const EMOJI =
 	process.env.MESSAGE && process.env.MESSAGE.trim().length > 0
 		? process.env.MESSAGE
@@ -8,8 +10,8 @@ export const EMOJI =
 export const VERSION = pkg.version ?? "0.0.0";
 export const DESTROY_DATABASE = process.env.DESTROY === "true";
 export const INFO_EMOJI = {
-	discord: process.env.DISCORD ?? "💻",
+	discord: process.env.DISCORD ?? "1125076859881791590",
 	docs: process.env.DOCS ?? "📚",
-	github: process.env.GITHUB_EMOJI ?? "🐙",
-	kofi: process.env.KOFI ?? "☕",
+	github: process.env.GITHUB_EMOJI ?? "1125070689146261544",
+	kofi: process.env.KOFI ?? "1125076879683100692",
 };
