671e92fe9d5fd4f355693910c7937f7550eb2f8b
Mara-Li <lili.simonetti@outlook.fr>
2026-01-17 14:41:30 +0100

    perf: register command globally

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,7 +1,6 @@
 import type { Client } from "discord.js";
 import { ALL_COMMANDS } from "../commands";
 import { VERSION } from "../interfaces";
-import { runWithConcurrency } from "../utils/concurrency";
 
 export default (client: Client): void => {
 	client.on("clientReady", async () => {
@@ -10,24 +9,7 @@ export default (client: Client): void => {
 		console.info(`${client.user.username} is online; v.${VERSION}`);
 
 		const serializedCommands = ALL_COMMANDS.map((command) => command.data.toJSON());
-		const guilds = Array.from(client.guilds.cache.values());
-		if (guilds.length === 0) {
-			console.warn("Bot is not in any guilds; skipping command registration.");
-			return;
-		}
-
-		console.info(`Registering commands on ${guilds.length} servers...`);
-		const tasks = guilds.map((guild) => {
-			return async () => {
-				try {
-					await guild.commands.set(serializedCommands);
-				} catch (error) {
-					console.error(`[${guild.name}] Failure on registering commands:`, error);
-				}
-			};
-		});
-
-		await runWithConcurrency(tasks, 5);
+		await client.application.commands.set(serializedCommands);
 		console.info("Done registering commands.");
 	});
 };
